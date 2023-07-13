514754b338e107a0526451ed91a5eed955e1ebb8
JanSafronov <jansafronov@gmail.com>
2023-07-13 22:50:04 +0300

    Add purge comment

--- server/index.js
+++ server/index.js
@@ -55,7 +55,13 @@ app.listen(port, () => {
   startCronJobs()
 })
 
-
+/**
+ *  Purge require cache on requests for "server side HMR". This won't let you have
+ *  in-memory objects between requests in development, alternatively you can set up
+ *  nodemon/pm2-dev to restart the server on file changes, but then you'll have to
+ *  reconnect to databases/etc on each change. We prefer the DX of this, so we've
+ *  included it for you by default
+ */
 function purgeRequireCache() {
   for (let key in require.cache) {
     if (key.startsWith(BUILD_DIR)) {
