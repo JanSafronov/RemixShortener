c94471a912d19de6978c35cd408ce798fad699cb
JanSafronov <jansafronov@gmail.com>
2023-07-13 22:58:42 +0300

    Create session storage (literally)

---
+++ app/session.server.ts
@@ -0,0 +1,43 @@
+import type { SessionIdStorageStrategy } from '@remix-run/node'
+import { createSessionStorage, redirect } from '@remix-run/node'
+import {
+  createSession,
+  deleteSession,
+  doesSessionExist,
+  getSessionById,
+  updateSession,
+} from './models/session'
+
+const DEFAULT_MAX_AGE = 3600
+const DEFAULT_SECRET = 's3cr3t'
+const INVALID_SESSION_REDIRECT = '/'
+
+function createDatabaseSessionStorage(
+  cookie: SessionIdStorageStrategy['cookie']
+) {
+  return createSessionStorage({
+    cookie,
+    async createData(data, expires) {
+      const { id } = await createSession(
+        data.userId,
+        expires || new Date(Date.now() + DEFAULT_MAX_AGE)
+      )
+      return id
+    },
+    async readData(id) {
+      const sessionData = await getSessionById(id)
+      return sessionData ?? { invalid: true }
+    },
+    async updateData(id, data, expires) {
+      await updateSession(
+        id,
+        data.userId,
+        expires || new Date(Date.now() + DEFAULT_MAX_AGE)
+      )
+    },
+    async deleteData(id) {
+      if (!(await doesSessionExist(id))) return
+      await deleteSession(id)
+    },
+  })
+}
