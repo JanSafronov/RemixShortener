c36387a5670cee530457a4c4fcbd3d26c799424d
JanSafronov <jansafronov@gmail.com>
2023-07-13 23:10:05 +0300

    Create theme control and Link

---
+++ app/routes/components/ThemeToggle.tsx
@@ -0,0 +1,34 @@
+import Brightness2Icon from '@mui/icons-material/Brightness2'
+import Brightness7Icon from '@mui/icons-material/Brightness7'
+import { IconButton, Tooltip } from '@mui/material'
+import { useFetcher, useMatches } from '@remix-run/react'
+import type { LoaderData as RootLoaderData } from '~/root'
+
+export default function ThemeToggle() {
+  const rootLoaderData = useMatches().find(match => match.id === 'root')
+    ?.data as RootLoaderData | undefined
+  const fetcher = useFetcher()
+
+  return (
+    <fetcher.Form
+      method="post"
+      action="/set-theme"
+      style={{ display: 'inline-block' }}
+    >
+      <input
+        type="hidden"
+        name="theme"
+        value={rootLoaderData?.themeName === 'dark' ? 'light' : 'dark'}
+      />
+      <Tooltip title="Toggle theme">
+        <IconButton type="submit" aria-label="Toggle theme">
+          {rootLoaderData?.themeName === 'dark' ? (
+            <Brightness2Icon />
+          ) : (
+            <Brightness7Icon />
+          )}
+        </IconButton>
+      </Tooltip>
+    </fetcher.Form>
+  )
+}
